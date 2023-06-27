13cc5cdb848d9a08ed3e75233c554392553c4515
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2023-06-27 16:40:14 +0800

    simplified create_table function

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ from math import ceil
 import numpy as np
 import pandas as pd
 
-def create_table(surveys: list, rates: list, survey_title: list, to_df=False):
+def create_table(surveys: list, rates: list, survey_title: list):
     # using the c,s,e formulation for surveys with d = e - s + 1
     n = len(surveys)
     m = max([e for c,s,e in surveys])
@@ -15,15 +15,14 @@ def create_table(surveys: list, rates: list, survey_title: list, to_df=False):
         agents = ceil(c / (r * d))
         table[idx][s:e] = agents
 
-    if to_df:
-        survey_names = [*survey_title, 'Required Agents']
-        table = np.vstack((table, table.sum(axis=0)))
-        table = pd.DataFrame(table, columns=[f'Day {i}' for i in range(1, m+1)]).astype(int)
-        table.insert(loc=0, column='Surveys', value=survey_names)
-        table.set_index('Surveys', inplace=True)
-        return table
-    else:
-        return table
+    survey_names = [*survey_title, 'Required Agents']
+    table = np.vstack((table, table.sum(axis=0)))
+    table = pd.DataFrame(table, columns=[f'Day {i}' for i in range(1, m+1)]).astype(int)
+    table.insert(loc=0, column='Surveys', value=survey_names)
+    table.set_index('Surveys', inplace=True)
+    return table
+
+def view_alternative(df: pd.DataFrame)
 
 def left_align(s, props='text-align: center;'):
     return props
