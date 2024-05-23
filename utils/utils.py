0fd999bec57a741e078e44aef2d766a253c375bf
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2024-05-23 10:26:07 +0800

    added explanation and example code below

--- utils/utils.py
+++ utils/utils.py
@@ -167,3 +167,131 @@ def get_const(file_path: str):
 
 def left_align(s, props='text-align: center;'):
     return props
+
+# Explanation 
+"""
+import copy
+
+# SOMEHOW NEED TO ENSURE EXACTLY ONE ITEM FROM EACH GROUP IS SELECTED
+def knapsack_multichoice_onepick(weights, values, max_weight):
+    if len(weights) == 0:
+        return 0
+
+    # stores the `best` items within their `weight classes`
+    # the `weight class` is the collection of all items that share the same weight
+    # the `best` item within their `weight class` is the item with the largest value
+    last_array = [-1 for _ in range(max_weight + 1)]
+    # stores the location of the `best` items within the group
+    last_path = [[] for _ in range(max_weight + 1)]
+
+    # Runs through all weights in `group_0`
+    print(f"\n{'='*10}\ngroup_0\n{'='*10}")
+    for i in range(len(weights[0])):
+
+        print(f"\nweights[0][{i}]", weights[0][i], f"< {max_weight} max_weight")
+        if weights[0][i] < max_weight:
+            # `item i` is first sorted into it's `weight class` (by placing it into the weights[0][i]-th index of the `last array``)
+            # then compare the value of `item i` to the current value stored in the weights[0][i]-th position of the `last array`
+            # and discard the least valuable `item`
+            # store the position of `item i` in the last_path using the `weight class` index (weights[0][i]-th index)
+
+            print(f"\tlast_array[weights[0][{i}]]:", last_array[weights[0][i]], f"< value[0][{i}]", values[0][i])
+            if last_array[weights[0][i]] < values[0][i]:
+                last_array[weights[0][i]] = values[0][i]
+                last_path[weights[0][i]] = [(0, i)]
+
+                print("\t\tlast_array:", last_array, "last_path:", last_path)
+
+    # Iterate through all other groups and repeat the same process as before for the other groups
+    for i in range(1, len(weights)):
+        current_array = [-1 for _ in range(max_weight + 1)]
+        current_path = [[] for _ in range(max_weight + 1)]
+        print(f"\n{'='*10}\ngroup_{i}\n{'='*10}")
+
+        # Runs through all weights in `group_i`
+        for j in range(len(weights[i])):
+            # iterates through the `weight classes` from weights[i][j] to max_weight
+            for k in range(weights[i][j], max_weight + 1):
+
+                print(f"\nlast_array[{k - weights[i][j]}]:", last_array[k - weights[i][j]], "> 0")
+                # check the last_weight for any `items` with weights less than or equal to `max_weight` - weight[i][j]
+                # this is so that the sum of `j` (the weight of the item we are looking for) and the `weight` of the item
+                # that was previously selected will be less than the `max_weight`
+                if last_array[k - weights[i][j]] > 0:
+
+                    print(f"\tcurrent_array[{k}]", current_array[k], "<", f"last_array[{k - weights[i][j]}] + values[{i}][{j}]", last_array[k - weights[i][j]], values[i][j])
+                    # if the total `value` of the items can be improved by including the currently selected `item`
+                    # add the `item`, update the `current_path` and value in the `current_array`
+                    if current_array[k] < last_array[k - weights[i][j]] + values[i][j]:
+                        current_array[k] = last_array[k - weights[i][j]] + values[i][j]
+
+                        # append the old path from `last_path` to the `current_path`
+                        current_path[k] = copy.deepcopy(last_path[k - weights[i][j]])
+                        current_path[k].append((i, j))
+
+                        print("\t\tcurrent_array", current_array, "current_path", current_path)
+
+        last_array = current_array
+        last_path = current_path
+        print("\nlast_array", last_array, "last_path", last_path)
+
+
+    solution, index_path = get_onepick_solution(last_array, last_path)
+
+    return solution, index_path
+
+
+def get_onepick_solution(scores, paths):
+    scores_paths = list(zip(scores, paths))
+    scores_paths_by_score = sorted(scores_paths, key=lambda tup: tup[0],
+                                    reverse=True)
+
+    return scores_paths_by_score[0][0], scores_paths_by_score[0][1]
+"""
+
+# Worked example
+"""
+'''
+value = total number of days
+weight = number of call agents
+'''
+lst = [(300, 15, 0), (1100, 12, 0), (700, 14, 0), (900, 9, 0), (400, 20, 0), (600, 18, 0), (700, 17, 0)]
+max_extension = 10
+daily_agents = lambda c, r, d, x: ceil(c / (r * (d + x)))
+
+problem_dict = [dict.fromkeys([]) for _ in lst]
+for idx, (cr, rate, day) in enumerate(lst):
+    for extension in range(max_extension):
+        agents = daily_agents(cr, rate, day, extension+1)
+        if agents not in problem_dict[idx]:
+            problem_dict[idx][max_extension-extension] = agents
+# values, weights = list(zip(*[list(zip(*category.items())) for category in problem_dict]))
+
+print(problem_dict)
+print([category.items() for category in problem_dict])
+print([list(zip(*category.items())) for category in problem_dict])
+print(list(zip(*[list(zip(*category.items())) for category in problem_dict])))
+
+values = []
+weights = []
+for survey in problem_dict:
+    values.append(list(survey.keys()))
+    weights.append(list(survey.values()))
+print(values, weights)
+# manpower = 40
+
+# problem_dict
+# W = 40
+# k = [list(d.keys()) for d in problem_dict]
+
+# sols = knapsack_multichoice_onepick(weights, values, W)
+# agent_ext_lst = []
+# for idx, (score, path) in enumerate(sols):
+#     ext = [max_extension-k[idx][elem] for idx, elem in enumerate([tup[1] for tup in path])]
+#     ags = [tup[2] for tup in path]
+#     agent_ext_lst.append((ags, ext))
+
+# # if agent_ext_lst:
+# #     print(agent_ext_lst[0])
+# #     print([(cr - rate*agents*(days+1)) for (cr, rate, _), agents, days in zip(lst, *agent_ext_lst[0])])
+"""
